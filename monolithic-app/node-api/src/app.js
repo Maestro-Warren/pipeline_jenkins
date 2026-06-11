fef547ce89e5a19d850c9dc254d341860155effd
Maestro-Warren <kajecaponne@gmail.com>
2026-06-11 15:07:43 +0100

    commit3

--- monolithic-app/node-api/src/app.js
+++ monolithic-app/node-api/src/app.js
@@ -38,7 +38,7 @@ app.post('/api/users', (req, res) => {
 if (require.main === module) {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running this port ${PORT}`);
   });
 }
 
